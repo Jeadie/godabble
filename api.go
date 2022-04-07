dd27db7a5d290c3f683dd503be39671df2ba0d82
Jeadie <jackeadie@duck.com>
2022-04-07 20:33:40 +1000

    add testing to api endpoints

--- api.go
+++ api.go
@@ -26,15 +26,110 @@ type Api struct {
 	version uint
 }
 
-// Construct an api object to interact with dabble.com. Version number should be at least 1.
-func ConstructApi(v uint) *Api {
+// Construct an Api to interact with dabble.com.
+func Construct() *Api {
 	return &Api{
 		client:  &http.Client{},
-		version: v,
+		version: 1,
 	}
 }
 
-func (api *Api) GetRaw(uri string) ([]byte, error) {
+// Me endpoint. Sparse fields if unauthenticated.
+func (api *Api) Me() (*Me, error) {
+	me := &Me{}
+	return me, api.getDecode("/me", me)
+}
+
+// Home page used by dabble.com
+func (api *Api) Home() (*Home, error) {
+	h := &Home{}
+	return h, api.getDecode("/pages/home", h)
+}
+
+// Categories found on the the categories page. Importantly, not the category page itself (which is /pages/categories)
+func (api *Api) Categories() ([]Category, error) {
+	c := &Categories{}
+	err := api.getDecode("/categories", c)
+	if err != nil {
+		return []Category{}, err
+	}
+	if len(c.ErrorMessage) > 0 {
+		return []Category{}, fmt.Errorf("received error in dabble.com response, %s", c.ErrorMessage)
+	}
+	return c.Categories, nil
+}
+
+// CategoryPage for a single dabble category. Importantly slug is a single `/%s`. Subcategories, i.e. `/%s/%s` Don't
+// have the same page structure.
+func (api *Api) CategoryPage(slug string) (*CategoryPage, error) {
+	n := &CategoryPage{}
+	err := api.getDecode(fmt.Sprintf("/pages/category?slug=/category/%s", slug), n)
+	if err == nil && len(n.ErrorMessage) > 0 {
+		return n, fmt.Errorf("received error in dabble.com response, %s", n.ErrorMessage)
+	}
+	return n, err
+}
+
+// Comments from an arbitrary reference.
+func (api *Api) Comments(rId, rType string, start, limit uint) (*Comments, error) {
+	c := &Comments{}
+	err := api.getDecode(fmt.Sprintf(
+		"/comments?reference_id=%s&reference_type=%s&cursor=%d&limit=%d", rId, rType, start, limit,
+	), c)
+	if err == nil && len(c.ErrorMessage) > 0 {
+		return c, fmt.Errorf("received error in dabble.com response, %s", c.ErrorMessage)
+	}
+	return c, err
+}
+
+// NewsPage is a page for a single news article with related content data (i.e. related tags, tickers, etc).
+func (api *Api) NewsPage(slug string) (*NewsPage, error) {
+	n := &NewsPage{}
+	err := api.getDecode(fmt.Sprintf("/pages/news?slug=/news/%s", slug), n)
+	if err == nil && len(n.ErrorMessage) > 0 {
+		return n, fmt.Errorf("received error in dabble.com response, %s", n.ErrorMessage)
+	}
+	return n, err
+}
+
+// PortfolioPage for a specific portfolio.
+func (api *Api) PortfolioPage(slug string) (*PortfolioPage, error) {
+	p := &PortfolioPage{}
+	err := api.getDecode(fmt.Sprintf("/pages/portfolio?slug=/portfolio/%s", slug), p)
+	if err == nil && len(p.ErrorMessage) > 0 {
+		return p, fmt.Errorf("received error in dabble.com response, %s", p.ErrorMessage)
+	}
+	return p, err
+}
+
+// Rankings of the largest gaining and losing portfolios
+func (api *Api) Rankings() (*Rankings, error) {
+	r := &Rankings{}
+	return r, api.getDecode("/pages/rankings", r)
+}
+
+// Stock page for a specific stock. stockKey is not a slug, but rather the end stock tag. e.g. AAPL.
+func (api *Api) Stock(stockKey string) (*Stock, error) {
+	s := &Stock{}
+	return s, api.getDecode(fmt.Sprintf("/pages/ticker?slug=/stocks/%s", stockKey), s)
+}
+
+// Tags associated with the home page. Tags on a category page in specific are found in CategoryPage.Tags
+func (api *Api) Tags() []Tag {
+	t := &Tags{}
+	err := api.getDecode("/tags", t)
+	if err != nil {
+		return []Tag{}
+	}
+	return t.Tags
+}
+
+// TODO
+//	https://api.dabble.com/v1/charts/ticker?id=c4s0v1m73jrtd86rcil0&fidelity=1y
+//  https://api.dabble.com/v1/pages/ticker?slug=/stocks/aapl
+//  https://api.dabble.com/v1/pages/ticker?slug=/crypto/x-adausd
+
+func (api *Api) getRaw(uri string) ([]byte, error) {
 	res, err := api.client.Get(fmt.Sprintf(
 		"https://api.dabble.com/v%d%s", api.version, uri,
 	))
@@ -49,88 +144,10 @@ func (api *Api) GetRaw(uri string) ([]byte, error) {
 	return body, nil
 }
 
-func (api *Api) GetDecode(uri string, obj any) error {
-	b, err := api.GetRaw(uri)
+func (api *Api) getDecode(uri string, obj any) error {
+	b, err := api.getRaw(uri)
 	if err != nil {
-		return errors.New("bad GetRaw" + err.Error())
+		return errors.New("bad getRaw" + err.Error())
 	} //err }
 	return json.Unmarshal(b, obj)
-}
-
-func (api *Api) Me() (*Me, error) {
-	me := &Me{}
-	return me, api.GetDecode("/me", me)
-}
-func (api *Api) Home() (*Home, error) {
-	h := &Home{}
-	return h, api.GetDecode("/pages/home", h)
-}
-
-func (api *Api) Categories() ([]Category, error) {
-	c := &Categories{}
-	err := api.GetDecode("/categories", c)
-	if err != nil {
-		return []Category{}, err
-	}
-	if len(c.ErrorMessage) > 0 {
-		return []Category{}, fmt.Errorf("received error in dabble.com response, %s", c.ErrorMessage)
-	}
-	return c.Categories, nil
-}
-
-func (api *Api) CategoryPage(slug string) (*CategoryPage, error) {
-	n := &CategoryPage{}
-	err := api.GetDecode(fmt.Sprintf("/pages/category?slug=/category/%s", slug), n)
-	if err == nil && len(n.ErrorMessage) > 0 {
-		return n, fmt.Errorf("received error in dabble.com response, %s", n.ErrorMessage)
-	}
-	return n, err
-}
-
-func (api *Api) Comments(rId, rType string, start, limit uint) (*Comments, error) {
-	c := &Comments{}
-	err := api.GetDecode(fmt.Sprintf(
-		"/comments?reference_id=%s&reference_type=%s&cursor=%d&limit=%d", rId, rType, start, limit,
-	), c)
-	if err == nil && len(c.ErrorMessage) > 0 {
-		return c, fmt.Errorf("received error in dabble.com response, %s", c.ErrorMessage)
-	}
-	return c, err
-}
-
-func (api *Api) NewsPage(slug string) (*NewsPage, error) {
-	n := &NewsPage{}
-	err := api.GetDecode(fmt.Sprintf("/pages/news?slug=/news/%s", slug), n)
-	if err == nil && len(n.ErrorMessage) > 0 {
-		return n, fmt.Errorf("received error in dabble.com response, %s", n.ErrorMessage)
-	}
-	return n, err
-}
-
-func (api *Api) PortfolioPage(slug string) (*PortfolioPage, error) {
-	p := &PortfolioPage{}
-	err := api.GetDecode(fmt.Sprintf("/pages/portfolio?slug=/portfolio/%s", slug), p)
-	if err == nil && len(p.ErrorMessage) > 0 {
-		return p, fmt.Errorf("received error in dabble.com response, %s", p.ErrorMessage)
-	}
-	return p, err
-}
-
-func (api *Api) Rankings() (*Rankings, error) {
-	r := &Rankings{}
-	return r, api.GetDecode("/pages/rankings", r)
-}
-
-func (api *Api) Stock(stockKey string) (*Stock, error) {
-	s := &Stock{}
-	return s, api.GetDecode(fmt.Sprintf("/pages/ticker?slug=/stocks/%s", stockKey), s)
-}
-
-func (api *Api) Tags() []Tag {
-	t := &Tags{}
-	err := api.GetDecode("/tags", t)
-	if err != nil {
-		return []Tag{}
-	}
-	return t.Tags
 }
--- api_test.go
+++ api_test.go
@@ -4,10 +4,57 @@ import (
 	"testing"
 )
 
+// TestApi calls each endpoint in Api, and confirms JSON parsing is valid and no other errors occur.
 func TestApi(t *testing.T) {
-	api := ConstructApi(1)
-	_, err := api.Home()
+	api := Construct()
+
+	_, err := api.Me()
 	if err != nil {
-		t.Errorf(err.Error())
+		t.Errorf("failed to call Me, %s", err.Error())
+	}
+
+	_, err = api.Categories()
+	if err != nil {
+		t.Errorf("failed to call Categories, %s", err.Error())
+	}
+
+	_, err = api.CategoryPage("technology")
+	if err != nil {
+		t.Errorf("failed to call CategoryPage, %s", err.Error())
+	}
+
+	_, err = api.Comments("c8la7im73jrvpjmpo42g", "portfolio", 0, 5)
+	if err != nil {
+		t.Errorf("failed to call Comments, %s", err.Error())
+	}
+
+	_, err = api.NewsPage("elon-musk-says-domino-effect-led-to-twitter-button-from-him-selling-first-firm-in-the-90s-c97bc8e73jrtmrh0sgfg")
+	if err != nil {
+		t.Errorf("failed to call NewsPage, %s", err.Error())
+	}
+
+	_, err = api.PortfolioPage("bitcoin-only-100-bitcoin-c8fuq8m73jru622m1f50")
+	if err != nil {
+		t.Errorf("failed to call PortfolioPage, %s", err.Error())
+	}
+
+	_, err = api.Rankings()
+	if err != nil {
+		t.Errorf("failed to call Rankings, %s", err.Error())
+	}
+
+	_, err = api.Stock("AAPL")
+	if err != nil {
+		t.Errorf("failed to call Stock, %s", err.Error())
+	}
+
+	tags := api.Tags()
+	if len(tags) == 0 {
+		t.Errorf("no tags returned when calling /tags")
+	}
+
+	_, err = api.Home()
+	if err != nil {
+		t.Errorf("failed to call Home, %s", err.Error())
 	}
 }
